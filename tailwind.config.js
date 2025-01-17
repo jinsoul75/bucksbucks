4c821fc5b256f1715ef026405069ae59b08e17fa
jinsoul <wlsthf75@zetalux.co.kr>
2025-01-17 09:22:09 +0900

    fix: window에서 yarn berry 안되는 문제 해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         yellow: {
-          light: '#FEF3E2', // 연노랑색 (기존 primary)
-          dark: '#fab12f', // 진노랑색 (기존 tertiary)
-          hover: '#fa812f', // 진노랑색 (기존 tertiary)
+          light: "#FEF3E2", // 연노랑색 (기존 primary)
+          dark: "#fab12f",
+          hover: "#fa812f"
         },
         green: {
-          DEFAULT: '#155e13', // 녹색 (기존 secondary)
-        },
+          DEFAULT: "#155e13"
+        }
       },
       fontFamily: {
-        pretendard: ['Pretendard', 'sans-serif'],
-      },
-    },
+        pretendard: ["Pretendard", "sans-serif"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
